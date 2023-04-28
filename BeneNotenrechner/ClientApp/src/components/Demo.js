767cf2d10d4df5a71ff3152a8efa7ead81fea49a
= <mineking61442@gmail.com>
2023-04-28 16:56:44 +0200

    Projektdateien hinzufügen.

---
+++ BeneNotenrechner/ClientApp/src/components/Demo.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+
+export class Demo extends Component {
+    static displayName = Demo.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { currentText: 'FUCKKKK' };
+        this.incrementCounter = this.incrementCounter.bind(this);
+    }
+
+    incrementCounter(evt) {
+        const val = evt.target.value;
+        this.setState({
+            currentText: val
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Demo</h1>
+
+                <p>Im trying my best</p>
+
+                <p aria-live="polite">Current Text: <strong>{this.state.currentText}</strong></p>
+
+                <input type="input" class="form-control" onChange={evt => this.incrementCounter(evt)} value={this.state.currentText}/>
+            </div>
+        );
+    }
+}
